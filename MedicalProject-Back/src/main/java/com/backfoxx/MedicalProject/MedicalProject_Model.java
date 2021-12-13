ed437126d56f3c518ea48df0c4ade5ef87f36a49
BackFoxx <jorealbusiness@gmail.com>
2021-12-14 00:54:20 +0900

    기초적인 설정(Guava, Lombok) 완료

---
+++ MedicalProject-Back/src/main/java/com/backfoxx/MedicalProject/MedicalProject_Model.java
@@ -0,0 +1,13 @@
+package com.backfoxx.MedicalProject;
+
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@RequiredArgsConstructor
+public class MedicalProject_Model {
+
+    @NonNull
+    private String id;
+}
